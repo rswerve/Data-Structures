cf6ee50526e03aff26dd6a648ad608c2e60b1562
rswerve <rswerve@gmail.com>
2016-01-29 17:24:28 -0600

    finished linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,28 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+   if (list.head === null){
+    list.head = Node(value)
+    list.tail = Node(value)
+   } else {
+    list.tail = Node(value)
+    list.head.next = value
+   }
   };
 
   list.removeHead = function(){
+    var headValue = list.head.value
+    list.head.value = list.head.next
+    return headValue
   };
 
   list.contains = function(target){
+    for (var key in list){
+      if (list[key].value === target){
+        return true
+      }
+    }
+      return false
   };
 
   return list;
