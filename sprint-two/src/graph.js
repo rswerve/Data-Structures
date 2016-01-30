00fa545881f147b1a2b704b0e585d654a3a077ee
rswerve <rswerve@gmail.com>
2016-01-30 10:22:57 -0600

    finished graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,26 +3,20 @@
 // ###Graph Solution
 
 // Instantiate a new graph
-var Graph = function(value){
-  this[value] = [];
+var Graph = function(){
+  
 };
 
 // -----------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(value){
   this[value] = []
-
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(target){
-  for (var key in this){
-    if (key === target){
-      return true
-    }
-  }
-  return false
+  return this.hasOwnProperty(target)
 };
 
 // ------------------------
@@ -44,8 +38,14 @@ Graph.prototype.hasEdge = function(fromValue, toValue){
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromValue, toValue){
-  this[fromValue].push(toValue)
-  this[toValue].push(fromValue)
+  if (this.hasOwnProperty(fromValue) && this.hasOwnProperty(toValue)){
+    if (this[fromValue].indexOf(toValue) === -1){
+      this[fromValue].push(toValue)
+    } 
+    if (this[toValue].indexOf(fromValue) === -1){
+      this[toValue].push(fromValue)
+    }
+  }
 };
 
 // ------------------------
@@ -60,6 +60,9 @@ Graph.prototype.removeEdge = function(fromValue, toValue){
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb){
+  for (var key in this){
+    cb(key)
+  }
 };
 
 /*
