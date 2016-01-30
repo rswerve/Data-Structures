1e1a36302cf777dd00aa8acfcfdc3dd1143b6f1a
rswerve <rswerve@gmail.com>
2016-01-30 13:44:55 -0600

    all tests passing, remove not done

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,16 +4,37 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
+  var store = this._storage
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if (!store[i]){
+    store[i] = []
+    store[i].push([k,v])
+  }  else if  (store[i] && store[i][0][0] !== k){
+    store[i].push([k,v])
+  } else if (store[i][0][0] === k){
+    store[i][0][1] = v
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
+  var store = this._storage
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  // console.log(store[i][0])
+  if (store[i].length > 1){
+   for (var j = 0; j < store[i].length; j++){
+    if (store[i][j][0] === k){
+      return store[i][j][1]
+    }
+   }
+  } else {
+    return store[i][0][1]
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  var store = this._storage
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  store[i][0][1] = null
 };
 
 
