123a56437ee1ff58ca27e77a06614b9c3e881c21
rswerve <rswerve@gmail.com>
2016-01-30 07:47:08 -0600

    finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,28 +1,51 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  extend(newTree, treeMethods)
+  newTree.children = []
   return newTree;
 };
-
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
+var result = false
+if (this.value === target){
+  return true
+}
+for (var i = 0; i < this.children.length; i++){
+  if (this.children[i].value === target){
+    result = true
+  } 
+  if (this.children[i].children.length > 0 && result !== true){
+    result = this.children[i].contains(target)
+  }
+}
 
+return result
 };
 
+function extend (toObject, fromObject){
+  for (var key in fromObject){
+    toObject[key] = fromObject[key]
+  }
+}
+//this.children[i].children
+//functional-shared
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+// var aTree = Tree(5)
+// console.log(aTree)
+// aTree.addChild(4)
+// console.log(aTree)
+// aTree.addChild(3)
+// console.log(aTree)
+// aTree.children[0].addChild(7)
+// aTree.children[1].addChild(8)
+//console.log(aTree)
+//console.log(aTree)
+// console.log(aTree.children[0].children[0].value)
+// console.log(aTree.contains(8))
