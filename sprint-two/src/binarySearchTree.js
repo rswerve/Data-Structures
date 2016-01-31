e559e1f94ceb3774ceaff02a63fe33582d759295
rswerve <rswerve@gmail.com>
2016-01-30 18:28:53 -0600

    finished binary tree, adjusted spec for pseudoclassical

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,57 @@
 var BinarySearchTree = function(value){
+  this.value = value
+  this.left = null// <
+  this.right = null// >
+};
+
+
+
+BinarySearchTree.prototype.insert = function(value){
+  if (value < this.value){
+    if (this.left === null){
+      this.left = new BinarySearchTree(value)
+    } else {
+      this.left.insert(value)
+    }
+  }
+  if (value > this.value){
+    if (this.right === null){
+      this.right = new BinarySearchTree(value)
+    } else {
+      this.right.insert(value)
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function(target){
+if (this.value === target){
+  return true
+}
+if (target < this.value && this.left !== null){
+  if (target === this.left){
+    return true
+  } else {
+    return this.left.contains(target)
+  }
+}
+if (target > this.value && this.right !== null){
+  if (target === this.right){
+    return true
+  } else {
+    return this.right.contains(target)
+  }
+}
+return false
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb){
+cb(this.value)
+if (this.left !== null){
+  this.left.depthFirstLog(cb)
+}
+if (this.right !== null){
+  this.right.depthFirstLog(cb)
+}
 };
 
 
